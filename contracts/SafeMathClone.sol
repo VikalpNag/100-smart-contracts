e0c5d2a0272cd5cf7f9daa365c6d7804395a853e
Vikalp Nag <vikalpnag@gmail.com>
2025-04-07 15:33:00 +0530

    Safe math contract

--- contracts/SafeMathClone.sol
+++ contracts/SafeMathClone.sol
@@ -1,4 +1,16 @@
 // SPDX-License-Identifier : Unlicense
 pragma solidity ^0.8.0;
 
-library SafeMathClone {}
+library SafeMathClone {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a + b;
+        require(c >= a, "addition overFLow");
+        return c;
+    }
+
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {
+        uint256 c = a - b;
+        require(b <= a, "Subtraction Underflow");
+        return c;
+    }
+}
