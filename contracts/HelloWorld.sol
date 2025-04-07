18df33a4a1c80d93081634e3fd2606f6058ca1ce
Vikalp Nag <vikalpnag@gmail.com>
2025-04-07 13:15:37 +0530

    HelloWorld Contract: Test Passed

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.8.0;
 
-contract HelloWorl {
+contract HelloWorld {
     string public greet = "Hello! Web3 World";
 }
---
+++ test/helloWorld.test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("HelloWorld Contract", function () {
+  it("Returns the correct greeting", async () => {
+    const HelloWorld = await ethers.getContractFactory("HelloWorld");
+    const hello = await HelloWorld.deploy();
+    await hello.waitForDeployment();
+
+    expect(await hello.greet()).to.be.equal("Hello! Web3 World");
+  });
+});
