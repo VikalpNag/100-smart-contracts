a912f73e234c17742685a6e777d5d17e07b54325
Vikalp Nag <vikalpnag@gmail.com>
2025-04-08 01:28:59 +0530

    Initial supply test done on ERC20 Contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,16 +4,3 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.28",
 };
-
-// Setup: npm install alchemy-sdk
-// Github: https://github.com/alchemyplatform/alchemy-sdk-js
-import { Network, Alchemy } from "alchemy-sdk";
-
-// Optional config object, but defaults to demo api-key and eth-mainnet.
-const settings = {
-  apiKey: "DD110-QAku87oosyBq7BuofGvdzpEjcJ", // Replace with your Alchemy API Key.
-  network: Network.ETH_MAINNET, // Replace with your network.
-};
-const alchemy = new Alchemy(settings);
-
-alchemy.core.getBlock(15221026).then(console.log);
--- test/CustomERC20.test.js
+++ test/CustomERC20.test.js
@@ -1,2 +1,28 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+
+describe("CustomERC20", function () {
+  let erc, deployer;
+
+  beforeEach(async () => {
+    //defines owner
+    const [owner] = await ethers.getSigners();
+    deployer = owner;
+
+    const ERC20 = await ethers.getContractFactory("CustomERC20");
+    erc = await ERC20.deploy(1000);
+    await erc.waitForDeployment();
+  });
+
+  //Intial value test
+  it("Returns initial Supply", async () => {
+    const totalSupply = await erc.totalSupply();
+    const deployerBalance = await erc.balanceOf(deployer.address);
+    const decimals = await erc.decimals();
+
+    const expected = ethers.parseUnits("1000", decimals); //returns 1000*10**18
+
+    expect(totalSupply).to.be.equal(expected);
+    expect(deployerBalance).to.be.equal(expected);
+  });
+});
