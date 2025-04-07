a28414fe6a0b815d3237a244e90f10f515a828a5
Vikalp Nag <vikalpnag@gmail.com>
2025-04-07 22:50:26 +0530

    constructor created

--- contracts/CustomERC20.sol
+++ contracts/CustomERC20.sol
@@ -7,4 +7,20 @@ contract CustomERC20 {
     string public symbol = "MTK";
     uint8 public decimals = 18;
     uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance;
+
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 amount
+    );
+
+    constructor(uint256 _initialSupply) {
+        totalSupply = _initialSupply * (10 ** uint256(decimals));
+        balanceOf[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
 }
