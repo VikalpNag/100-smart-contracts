246e579191b251f255ef5e30da3ad9199e66ecc9
Vikalp Nag <vikalpnag@gmail.com>
2025-04-07 22:19:19 +0530

    var declaration on customERC20

--- contracts/CustomERC20.sol
+++ contracts/CustomERC20.sol
@@ -1,4 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CustomERC20 {}
+contract CustomERC20 {
+    //Variables declaration
+    string public name = "MyToken";
+    string public symbol = "MTK";
+    uint8 public decimals = 18;
+    uint256 public totalSupply;
+}
