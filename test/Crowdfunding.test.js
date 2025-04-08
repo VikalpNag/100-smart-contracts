e157a3097a6231bdb55573806b7e66ed94a23a04
Vikalp Nag <vikalpnag@gmail.com>
2025-04-08 22:48:34 +0530

    Crowdfunding() created

---
+++ test/Crowdfunding.test.js
@@ -0,0 +1,25 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Crowd funding", function () {
+  let crowdfunding;
+  let creator, backer1, backer2;
+  const goalAmount = ethers.parseEther("3"); // 3 ETH
+  const duration = 60 * 60 * 24; // 1 Day in seconds
+
+  beforeEach(async () => {
+    [creator, backer1, backer2] = await ethers.getSigners();
+
+    const Crowdfunding = await ethers.getContractFactory("Crowdfunding");
+    crowdfunding = await Crowdfunding.connect(creator).deploy(
+      duration,
+      goalAmount
+    );
+    await crowdfunding.waitForDeployment();
+  });
+
+  it("Should initialize with correct value", async () => {
+    expect(await crowdfunding.creator()).to.be.equal(creator.address);
+    expect(await crowdfunding.goal()).to.be.equal(goalAmount);
+  });
+});
