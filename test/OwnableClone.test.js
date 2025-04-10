ea19acafec280cdfbeae404cef3486ea4671c9d1
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 11:39:56 +0530

    deployer as owner test passed

--- test/OwnableClone.test.js
+++ test/OwnableClone.test.js
@@ -3,4 +3,15 @@ const { ethers } = require("hardhat");
 
 describe("Ownable Clone", function () {
   let ownable, owner, addr1;
+
+  beforeEach(async () => {
+    [owner, addr1] = await ethers.getSigners();
+    const OwnableClone = await ethers.getContractFactory("OwnableClone");
+    ownable = await OwnableClone.connect(owner).deploy();
+    await ownable.waitForDeployment();
+  });
+
+  it("Set the deployer as owner", async () => {
+    expect(await ownable.owner()).to.be.equal(owner.address);
+  });
 });
