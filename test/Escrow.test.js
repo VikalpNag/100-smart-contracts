dad75ac31622526f0e53d144df718d43800df319
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 10:40:01 +0530

    releaseFunds test passed

--- test/Escrow.test.js
+++ test/Escrow.test.js
@@ -21,4 +21,17 @@ describe("Escrow", function () {
     const balance = await ethers.provider.getBalance(escrow.target);
     expect(balance).to.be.equal(ethers.parseEther("1"));
   });
+
+  it("Returns funds withrawal", async () => {
+    //depositing funds by buyer
+    await escrow.connect(buyer).deposit({ value: ethers.parseEther("1") });
+    const balanceBefore = await ethers.provider.getBalance(escrow.target);
+
+    // Inspector releases funds to seller
+    await escrow.connect(inspector).releaseFunds();
+    const balanceAfter = await ethers.provider.getBalance(escrow.target);
+
+    expect(balanceBefore).to.be.equal(ethers.parseEther("1"));
+    expect(balanceAfter).to.be.equal(ethers.parseEther("0"));
+  });
 });
