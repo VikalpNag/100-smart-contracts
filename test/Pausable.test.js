ddf7057b1a453f546317a8811418e91cffc0d306
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 19:59:37 +0530

    Pause/Unpause Test passed for onlyowner

--- test/Pausable.test.js
+++ test/Pausable.test.js
@@ -10,4 +10,18 @@ describe("Pausable contract", function () {
     pausable = await Pausable.deploy();
     await pausable.waitForDeployment();
   });
+
+  it("Returns OnlyOwner to pause/unpause", async () => {
+    await expect(pausable.connect(other).pause()).to.be.revertedWith(
+      "Not the owner"
+    );
+    await pausable.connect(owner).pause();
+    expect(await pausable.paused()).to.equal(true);
+
+    await expect(pausable.connect(other).unPause()).to.be.revertedWith(
+      "Not the owner"
+    );
+    await pausable.connect(owner).unPause();
+    expect(await pausable.paused()).to.equal(false);
+  });
 });
