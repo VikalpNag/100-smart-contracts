ff008e2d69005589257d02199e3e9e9807745436
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 09:46:17 +0530

    constructor created

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -10,4 +10,14 @@ contract Escrow {
     bool public isFunded;
     bool public isRealesed;
     bool public isRefunded;
+
+    constructor(address _buyer, address _seller, address _inspector) {
+        require(_buyer != address(0), "Invalid buyer");
+        require(_seller != address(0), "Innvalid Seller");
+        require(_inspector != address(0), "Invalid Inspector");
+
+        buyer = _buyer;
+        seller = _seller;
+        inspector = _inspector;
+    }
 }
