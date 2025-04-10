8369d2543265ec330dc86d9ac4f8da932e6bec6b
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 19:55:41 +0530

    added beforeEach funcn

--- test/Pausable.test.js
+++ test/Pausable.test.js
@@ -1,4 +1,13 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-describe("Pausable contract", function () {});
+describe("Pausable contract", function () {
+  let owner, pausable, other;
+
+  beforeEach(async () => {
+    [owner, other] = await ethers.getSigners();
+    const Pausable = await ethers.getContractFactory("Pausable");
+    pausable = await Pausable.deploy();
+    await pausable.waitForDeployment();
+  });
+});
