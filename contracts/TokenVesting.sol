c28e82245250456dc4a290f24be51ea0b962822e
Vikalp Nag <vikalpnag@gmail.com>
2025-04-10 18:38:26 +0530

    COntructor created

--- contracts/TokenVesting.sol
+++ contracts/TokenVesting.sol
@@ -12,4 +12,29 @@ contract TokenVesting {
     uint256 public immutable totalAmount;
 
     uint256 public released;
+
+    constructor(
+        address _token,
+        address _beneficiary,
+        uint256 _start,
+        uint256 _duration,
+        uint256 _amount
+    ) {
+        require(_token != address(0), "Invalid token address");
+        require(_beneficiary != address(0), "Invalid beneficiary address");
+        require(_duration > 0, "Duration must be 0");
+
+        token = IERC20(_token);
+        beneficiary = _beneficiary;
+        start = _start;
+        duration = _duration;
+        totalAmount = _amount;
+    }
+
+    function release() public {
+        uint256 unreleased = vestedAmount() - released;
+        require(unreleased > 0, "No tokens to release");
+
+        released += unreleased;
+    }
 }
