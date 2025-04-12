8484d1b1baf04859528335d534db18de1de17b28
Vikalp Nag <vikalpnag@gmail.com>
2025-04-12 20:18:18 +0530

    created vars and constructor

--- contracts/17MyERC721.sol
+++ contracts/17MyERC721.sol
@@ -4,4 +4,11 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyERC721 is ERC721URIStorage, Ownable {}
+contract MyERC721 is ERC721URIStorage, Ownable {
+    uint256 public nextTokenId;
+
+    constructor(
+        string memory name,
+        string memory symbol
+    ) ERC721(name, symbol) {}
+}
