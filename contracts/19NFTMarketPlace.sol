ebde454038ec6a7d496a13f7633e6c1330027d8f
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 01:50:22 +0530

    initiated nftMarketplaces

---
+++ contracts/19NFTMarketPlace.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/security/ReentrancyGaurd.sol";
+
+contract NFTMarketplace is ReentrancyGaurd {
+    struct Listing {
+        address seller;
+        uint256 price;
+    }
+}
