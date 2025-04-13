f4f639dd9aed2b10419f273fd46749f2febba48c
Vikalp Nag <vikalpnag@gmail.com>
2025-04-13 14:55:18 +0530

    created vars

--- contracts/18MyERC1155.sol
+++ contracts/18MyERC1155.sol
@@ -3,3 +3,8 @@ pragma solidity ^0.8.0;
 
 import "@openzepplin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzepplin/contracts/access/Ownable.sol";
+
+contract MyERC1155 is ERC1155, Ownable {
+    uint256 public constant GOLD = 1;
+    uint256 public constant SILVER = 2;
+}
