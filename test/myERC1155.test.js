78e09bb0955f30391e3ed4d4478e011a895e57af
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 01:32:36 +0530

    test passed for mint()

--- test/myERC1155.test.js
+++ test/myERC1155.test.js
@@ -15,4 +15,9 @@
 //   it("Should Deploy with correct uri", async () => {
 //     expect(await contract.uri(1)).to.be.equal(uri);
 //   });
+
+//   it("Owner can mint single token", async () => {
+//     await contract.connect(owner).mint(user.address, 1, 100, "0x");
+//     expect(await contract.balanceOf(user.address, 1)).to.equal(100);
+//   });
 // });
