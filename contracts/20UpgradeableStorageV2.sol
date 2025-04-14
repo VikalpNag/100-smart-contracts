12a489cc4ff7afecbf0baf8c385330c2ff05c7a0
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 23:16:56 +0530

    created getValue() function

--- contracts/20UpgradeableStorageV2.sol
+++ contracts/20UpgradeableStorageV2.sol
@@ -7,4 +7,8 @@ contract UpgradeableStorageV2 {
     function setValue(uint256 _value) external {
         value = _value * 2;
     }
+
+    function getValue() external view returns (uint256) {
+        return value;
+    }
 }
