8d2fa8da65141f6f364d8c79cb2fb886e89dc430
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 11:35:50 +0530

    created vars and beforeEach for test

--- test/nftMarketplace.test.js
+++ test/nftMarketplace.test.js
@@ -1,6 +1,22 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("NFT MarketPlace", function () {
-  //code
-});
+// describe("NFT MarketPlace", function () {
+//   let NFT, nft, MarketPlace, marketplace, owner, seller, buyer;
+
+//   beforeEach(async () => {
+//     [owner, seller, buyer] = await ethers.getSigners();
+
+//     NFT = await ethers.getContractFactory("MyERC721");
+//     nft = await NFT.deploy("TestNFT", "TNFT");
+//     await nft.waitForDeployment();
+
+//     Marketplace = await ethers.getContractFactory("NFTMarketplace");
+//     marketplace = await Marketplace.deploy();
+//     await marketplace.waitForDeployment();
+
+//     // Mint and approve NFT
+//     await nft.connect(seller).mint("ipfs://token1.json");
+//     await nft.connect(seller).setApprovalForAll(marketplace.target, true);
+//   });
+// });
