f149041c8d997444fb71876ed8c4f292b554e8da
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 22:25:42 +0530

    created function setValue()

--- contracts/20UpgradeableStorage.sol
+++ contracts/20UpgradeableStorage.sol
@@ -3,7 +3,11 @@ pragma solidity ^0.8.0;
 
 contract UpgradeableStorage {
     //Slot 0:uint256 value
-    uint265 public value;
+    uint256 public value;
     //Slot 1 :address admin(Only used in  logic for simulation,not needed in minimal proxy)
     address public admin;
+
+    function setValue(uint256 _value) external {
+        value = _value;
+    }
 }
