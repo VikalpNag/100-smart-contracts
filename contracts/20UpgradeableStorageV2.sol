f727c5918fc4d6201746994555d23fd4af0bb5a8
Vikalp Nag <vikalpnag@gmail.com>
2025-04-14 23:15:17 +0530

    created New file :UpgradeableStorageV2

---
+++ contracts/20UpgradeableStorageV2.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract UpgradeableStorageV2 {
+    uint256 public value;
+
+    function setValue(uint256 _value) external {
+        value = _value * 2;
+    }
+}
