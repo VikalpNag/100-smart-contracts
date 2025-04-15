a512db5ac3ecb8cc12ce323c9548711fef8ca219
Vikalp Nag <vikalpnag@gmail.com>
2025-04-15 22:28:39 +0530

    created AccessControlRoles

---
+++ contracts/21AccessControlRoles.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract AccessControlRoles is ERC20, Pausable, AccessControl {}
