430abc6c1c8a14ab0cd2c2b892121ef60e810c36
Vikalp Nag <vikalpnag@gmail.com>
2025-04-18 00:14:11 +0530

    created Vars for staked

--- contracts/24Staking.sol
+++ contracts/24Staking.sol
@@ -7,4 +7,14 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Staking is Ownable {
     IERC20 public immutable stakingToken;
     IERC20 public immutable rewardToken;
+
+    uint256 public rewardRate = 100; //Tokens per second
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 public totalStaked;
+    mapping(address => uint256) public balanceOf;
 }
