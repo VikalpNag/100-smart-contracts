f3e84b79467607813747d00a10d2dd68394ee8e4
Vikalp Nag <vikalpnag@gmail.com>
2025-04-18 03:19:17 +0530

    fixed bugs

--- contracts/24PMPStaking.sol
+++ contracts/24PMPStaking.sol
@@ -1,28 +1,29 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.0;
-
-// Missing the file ERC20PresetMinterPauser.sol is not
-// found in the specified path because OpenZeppelin v5.x+ removed the presets/ directory.
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract TestToken is ERC20Burnable, Ownable, ERC20Capped, ERC20Pausable {
+contract PMPToken is ERC20Burnable, ERC20Capped, ERC20Pausable, Ownable {
     constructor(
         string memory name,
         string memory symbol,
         uint256 cap
-    ) ERC20(name, symbol) ERC20Capped(cap) Ownable(msg.sender) {
-        _mint(msg.sender, cap);
-    }
+    ) ERC20(name, symbol) ERC20Capped(cap) Ownable(msg.sender) {}
 
+    // Pausable transfer logic
     function _update(
         address from,
         address to,
         uint256 value
-    ) internal override(ERC20, ERC20Pausable) {
+    ) internal override(ERC20, ERC20Pausable, ERC20Capped) {
         super._update(from, to, value);
+    }
+
+    // Do NOT override _mint anymore!
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount); // ERC20Capped internally checks cap
     }
 }
--- test/staking.test.js
+++ test/staking.test.js
@@ -7,7 +7,7 @@ describe("Staking contract", function () {
   beforeEach(async () => {
     [user, owner] = await ethers.getSigners();
 
-    const ERC20 = await ethers.getContractFactory("ERC20PresetMinterPauser");
+    const ERC20 = await ethers.getContractFactory("PMPToken");
     stakingToken = await ERC20.deploy("StakeToken", "STK");
     rewardToken = await ERC20.deploy("RewardToken", "RWD");
 
@@ -19,5 +19,20 @@ describe("Staking contract", function () {
 
     //fund staking contract with reward tokens
     await rewardToken.transfer(staking.target, ethers.parseEther("500"));
+  });
+
+  it("Allows staking and earning rewards", async () => {
+    await stakingToken
+      .connect(user)
+      .approve(staking.target, ethers.parseEther("100"));
+    await staking.connect(user).stake(ethers.parseEther("100"));
+
+    await ethers.provider.send("evm_increaseTime", [10]);
+    await ethers.provider.send("evm_mine");
+
+    await staking.connect(user).getReward();
+
+    const reward = await rewardToken.balanceOf(user.address);
+    expect(reward).to.be.gt(0);
   });
 });
