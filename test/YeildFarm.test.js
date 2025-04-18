37276cb846392b3a0351c60e5344c357cdd23ed3
Vikalp Nag <vikalpnag@gmail.com>
2025-04-18 08:10:10 +0530

    created vars and const

---
+++ test/YeildFarm.test.js
@@ -0,0 +1,14 @@
+// const { expect } = require("chai");
+// const { ethers } = require("ethers");
+
+// describe("Yeild Farm staking", function () {
+//   let owner, user;
+//   let yeildfarm, tokenA, tokenB;
+
+//   const initialSupply = ethers.parseEther("100000");
+//   const stakeAmount = ethers.parseEther("1000");
+
+//   const APY_A = 10; //10%
+//   const APY_B = 20; //20%
+
+// });
