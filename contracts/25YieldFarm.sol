65e4bd45b701335326bc01bfdf054520f87b607e
Vikalp Nag <vikalpnag@gmail.com>
2025-04-18 09:17:40 +0530

    fixed typo

--- contracts/25YieldFarm.sol
+++ contracts/25YieldFarm.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-abstract contract YeildFarm is Ownable {
+abstract contract YieldFarm is Ownable {
     struct StakeInfo {
         uint256 amount;
         uint256 startTime;
