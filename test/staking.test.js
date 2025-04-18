2a930acbe824ae5dcf46df48cea5082c768750cd
Vikalp Nag <vikalpnag@gmail.com>
2025-04-18 08:36:24 +0530

    #commented test cases

--- test/staking.test.js
+++ test/staking.test.js
@@ -1,44 +1,44 @@
-const { expect, use } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect, use } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("Staking contract", function () {
-  let staking, stakingToken, rewardToken, user, owner;
+// describe("Staking contract", function () {
+//   let staking, stakingToken, rewardToken, user, owner;
 
-  beforeEach(async () => {
-    [owner, user] = await ethers.getSigners();
+//   beforeEach(async () => {
+//     [owner, user] = await ethers.getSigners();
 
-    const ERC20 = await ethers.getContractFactory("TestToken");
+//     const ERC20 = await ethers.getContractFactory("TestToken");
 
-    // Add cap as 1 million tokens (converted to wei)
-    const cap = ethers.parseEther("1000000");
+//     // Add cap as 1 million tokens (converted to wei)
+//     const cap = ethers.parseEther("1000000");
 
-    stakingToken = await ERC20.deploy("StakeToken", "STK", cap);
-    rewardToken = await ERC20.deploy("RewardToken", "RWD", cap);
+//     stakingToken = await ERC20.deploy("StakeToken", "STK", cap);
+//     rewardToken = await ERC20.deploy("RewardToken", "RWD", cap);
 
-    // Mint tokens to user and owner
-    await stakingToken.mint(user.address, ethers.parseEther("1000"));
-    await rewardToken.mint(owner.address, ethers.parseEther("1000"));
+//     // Mint tokens to user and owner
+//     await stakingToken.mint(user.address, ethers.parseEther("1000"));
+//     await rewardToken.mint(owner.address, ethers.parseEther("1000"));
 
-    const Staking = await ethers.getContractFactory("Staking");
-    staking = await Staking.deploy(stakingToken.target, rewardToken.target);
+//     const Staking = await ethers.getContractFactory("Staking");
+//     staking = await Staking.deploy(stakingToken.target, rewardToken.target);
 
-    // Fund staking contract with reward tokens
+//     // Fund staking contract with reward tokens
 
-    await rewardToken.transfer(staking.target, ethers.parseEther("500"));
-  });
+//     await rewardToken.transfer(staking.target, ethers.parseEther("500"));
+//   });
 
-  it("Allows staking and earning rewards", async () => {
-    await stakingToken
-      .connect(user)
-      .approve(staking.target, ethers.parseEther("100"));
-    await staking.connect(user).stake(ethers.parseEther("100"));
+//   it("Allows staking and earning rewards", async () => {
+//     await stakingToken
+//       .connect(user)
+//       .approve(staking.target, ethers.parseEther("100"));
+//     await staking.connect(user).stake(ethers.parseEther("100"));
 
-    await ethers.provider.send("evm_increaseTime", [10]);
-    await ethers.provider.send("evm_mine");
+//     await ethers.provider.send("evm_increaseTime", [10]);
+//     await ethers.provider.send("evm_mine");
 
-    await staking.connect(user).getReward();
+//     await staking.connect(user).getReward();
 
-    const reward = await rewardToken.balanceOf(user.address);
-    expect(reward).to.be.gt(0);
-  });
-});
+//     const reward = await rewardToken.balanceOf(user.address);
+//     expect(reward).to.be.gt(0);
+//   });
+// });
