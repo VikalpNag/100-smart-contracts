7d38c40ed15eb58df3f214006f8b22140338bdef
Vikalp Nag <vikalpnag@gmail.com>
2025-04-20 23:10:14 +0530

    #1

--- test/TimeLock.test.js
+++ test/TimeLock.test.js
@@ -1,10 +1,17 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("TimeLock contract", function () {
-//   let owner, proposer, executor, target;
-//   let timelock;
+describe("TimeLock contract", function () {
+  let owner, proposer, executor, target;
+  let timelock;
 
-//   const MIN_DELAY = 3600; //1 hour
+  const MIN_DELAY = 3600; //1 hour
 
-// });
+  beforeEach(async () => {
+    [owner, proposer, executor, target] = await ethers.getSigners();
+
+    const TimeLock = await ethers.getContractFactory("TimeLock");
+    timelock = await TimeLock.deploy(owner.address, MIN_DELAY);
+    await timelock.waitForDeployment();
+  });
+});
