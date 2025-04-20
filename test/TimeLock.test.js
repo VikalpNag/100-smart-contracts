deef2373caff53b71c13940b65c937048f5c4d42
Vikalp Nag <vikalpnag@gmail.com>
2025-04-20 23:06:24 +0530

    initiated test file

---
+++ test/TimeLock.test.js
@@ -0,0 +1,10 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("TimeLock contract", function () {
+//   let owner, proposer, executor, target;
+//   let timelock;
+
+//   const MIN_DELAY = 3600; //1 hour
+
+// });
