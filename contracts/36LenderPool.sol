d168eb621c3f06da4a0e86e6265f64a189428b57
Vikalp Nag <vikalpnag@gmail.com>
2025-04-22 17:53:03 +0530

    declared vars and created constructor with token

--- contracts/36LenderPool.sol
+++ contracts/36LenderPool.sol
@@ -10,3 +10,12 @@ interface IFlashLoanReceiver {
         uint256 fee
     ) external;
 }
+
+contract LenderPool {
+    IERC20 public token;
+    uint256 public feeBasisPoints = 5; //0.05% fee
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+}
