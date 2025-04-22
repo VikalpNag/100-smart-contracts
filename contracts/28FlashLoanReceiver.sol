819c3b49dd240c67360eb7206e18a44fc32260bf
Vikalp Nag <vikalpnag@gmail.com>
2025-04-22 18:04:17 +0530

    created constructor

--- contracts/28FlashLoanReceiver.sol
+++ contracts/28FlashLoanReceiver.sol
@@ -6,4 +6,9 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract FlashLoanReceiver {
     address public pool;
     address public owner;
+
+    constructor(address _pool) {
+        pool = _pool;
+        owner = msg.sender;
+    }
 }
