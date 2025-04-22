305129cb8c9b36bbb86e62fd8e91973e304afc9f
Vikalp Nag <vikalpnag@gmail.com>
2025-04-22 18:21:30 +0530

    initiated test file

---
+++ test/FlashLoanReceiver.test.js
@@ -0,0 +1,25 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("Flash Loan Receiver", function () {
+//   let token, pool, receiver;
+//   let deployer;
+
+//   beforeEach(async () => {
+//     [deployer] = await ethers.getSigners();
+
+//     const Token = await ethers.getContractFactory("ACustomERC20");
+//     token = await Token.deploy("Token", "TOK", ethers.parseEther("100000"));
+
+//     const Pool = await ethers.getContractFactory("LenderPool");
+//     pool = await Pool.deploy(token.target);
+
+//     const Receiver = await ethers.getContractFactory("FlashLoanReceiver");
+//     receiver = await Receiver.deploy(pool.target);
+
+//     //Fund Pool
+//     await token.transfer(pool.target, ethers.parseEther("50000"));
+//     //Fund receiver with just enough to pay fee
+//     await token.transfer(receiver.target, ethers.parseEther("1000"));
+//   });
+// });
