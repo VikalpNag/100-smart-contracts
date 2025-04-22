b322db2624c1bf56cf40b4ce6c8af6635c8eed01
Vikalp Nag <vikalpnag@gmail.com>
2025-04-22 17:20:59 +0530

    test pass for creating and voting proposal

--- test/DAO.test.js
+++ test/DAO.test.js
@@ -29,4 +29,30 @@
 //     await token.connect(voter1).approve(dao.target, ethers.parseEther("10"));
 //     await token.connect(voter2).approve(dao.target, ethers.parseEther("10"));
 //   });
+
+//   it("Should Allow creating and voting on a proposal", async () => {
+//     const iface = new ethers.Interface(["function setValue(uint256)"]);
+//     const data = iface.encodeFunctionData("setValue", [123]);
+
+//     const tx = await dao
+//       .connect(voter1)
+//       .propose(dummy.target, 0, data, "Set Value to 123");
+
+//     const receipt = await tx.wait();
+//     const event = receipt.logs.find(
+//       (log) => log.eventName === "ProposalCreated"
+//     );
+//     const id = event.args[0];
+
+//     await dao.connect(voter1).vote(id, 1); //VoteType.For
+//     await dao.connect(voter2).vote(id, 2); //VoteType.Abstain
+
+//     //Fast forward time
+//     await ethers.provider.send("evm_increaseTime", [3 * 24 * 60 * 60 + 1]); //3 days
+//     await ethers.provider.send("evm_mine");
+
+//     await dao.connect(voter1).executeProposal(id);
+
+//     expect(await dummy.value()).to.equal(123n);
+//   });
 // });
