fc41054c909407f60a725af47cb0afd7e2e2063d
Vikalp Nag <vikalpnag@gmail.com>
2025-04-23 13:19:07 +0530

    created beforeEach()

--- test/TokenSwap.test.js
+++ test/TokenSwap.test.js
@@ -1,8 +1,27 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("Tokenswap contract", function () {
-  let Token, tokenA, tokenB, swap, owner, user;
-  const rate = 2; //1A=2B
-  const supply = ethers.parseEther("100000");
-});
+// describe("Tokenswap contract", function () {
+//   let Token, tokenA, tokenB, swap, owner, user;
+//   const rate = 2; //1A=2B
+//   const supply = ethers.parseEther("100000");
+
+//   beforeEach(async () => {
+//     [owner, user] = await ethers.getSigners();
+
+//     const ERC20 = await ethers.getContractFactory("ACustomERC20");
+//     tokenA = await ERC20.deploy("Token A", "TKA", supply);
+//     tokenB = await ERC20.deploy("Token B", "TKB", supply);
+
+//     const Swap = await ethers.getContractFactory("TokenSwap");
+//     swap = await Swap.deploy(tokenA.target, tokenB.target, rate);
+
+//     //Fund Swap with tokenB
+//     await tokenB.transfer(swap.target, ethers.parseEther("10000"));
+
+//     //Transfer tokenA to user
+//     await tokenA.transfer(user.address, ethers.parseEther("1000"));
+//     await tokenA.connect(user).approve(swap.target, ethers.parseEther("1000"));
+//     await tokenB.connect(user).approve(swap.target, ethers.parseEther("1000"));
+//   });
+// });
