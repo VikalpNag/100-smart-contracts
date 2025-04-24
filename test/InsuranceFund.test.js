501bfd78f2da75be60cc0d246d923158ee997236
Vikalp Nag <vikalpnag@gmail.com>
2025-04-24 11:13:36 +0530

    Initiated test file

---
+++ test/InsuranceFund.test.js
@@ -0,0 +1,13 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("InsuranceFund Contract", function () {
+//   let fund, owner, user1, user2;
+
+//   beforeEach(async () => {
+//     [owner, user1, user2] = await ethers.getSigners();
+
+//     const InsuranceFund = await ethers.getContractFactory("InsuranceFund");
+//     fund = await InsuranceFund.deploy();
+//   });
+// });
