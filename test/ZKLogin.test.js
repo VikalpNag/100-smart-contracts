dc9742ed89beed46d7d1214020bb6102af458919
Vikalp Nag <vikalpnag@gmail.com>
2025-04-25 10:55:47 +0530

    created beforeEach

--- test/ZKLogin.test.js
+++ test/ZKLogin.test.js
@@ -1,4 +1,24 @@
 // const { expect } = require("chai");
 // const { ethers } = require("hardhat");
+// const keccak256 = require("keccak256");
+// const { default: MerkleTree } = require("merkletreejs");
 
-// describe("ZKLogin contract", function () {});
+// describe("ZKLogin contract", function () {
+//   let zkLogin, owner, user1, user2;
+//   let tree, root;
+
+//   beforeEach(async () => {
+//     [owner, user1, user2] = await ethers.getSigners();
+
+//     const leaves = [user1.address, user2.address].map((addr) =>
+//       keccak256(addr)
+//     );
+
+//     tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+//     root = tree.getHexRoot();
+
+//     const ZKLogin = await ethers.getContractFactory("ZKLogin");
+//     zkLogin = await ZKLogin.deploy(root);
+//     await zkLogin.waitForDeployment();
+//   });
+// });
