21ad729645bf944214546a56788feb94b33eb537
Vikalp Nag <vikalpnag@gmail.com>
2025-04-25 10:24:07 +0530

    created constructor

--- contracts/37ZKLogin.sol
+++ contracts/37ZKLogin.sol
@@ -5,8 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract ZKLogin is Ownable {
-    bytes public merkleRoot;
+    bytes32 public merkleRoot;
     mapping(address => bool) public verifiedUsers;
 
     event Verified(address indexed user);
+
+    constructor(bytes32 _merkleRoot) {
+        merkleRoot = _merkleRoot;
+    }
 }
