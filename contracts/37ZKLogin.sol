8524b0703417939aa4646c9849dc82ca70829944
Vikalp Nag <vikalpnag@gmail.com>
2025-04-25 10:17:04 +0530

    created variables

---
+++ contracts/37ZKLogin.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract ZKLogin is Ownable {
+    bytes public merkleRoot;
+}
