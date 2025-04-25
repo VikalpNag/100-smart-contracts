4f7aad53d3d527a16309981ae2cbe7bccfab2209
Vikalp Nag <vikalpnag@gmail.com>
2025-04-25 17:30:52 +0530

    initiated test file,syntax error fix

--- contracts/38SoulboundNFT.sol
+++ contracts/38SoulboundNFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract SoulBound is ERC721, Ownable {
+contract SoulBoundNFT is ERC721, Ownable {
     uint256 private _tokenIdCounter;
 
     constructor(
---
+++ test/SoulboundNFT.test.js
@@ -0,0 +1,14 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("SouldBound Contract", function () {
+//   let soulbound, owner, user;
+
+//   beforeEach(async () => {
+//     [owner, user] = await ethers.getSigners();
+
+//     const SouldBound = await ethers.getContractFactory("SoulBoundNFT");
+//     soulbound = await SouldBound.deploy("Soulbound", "SBT");
+//     await soulbound.waitForDeployment();
+//   });
+// });
