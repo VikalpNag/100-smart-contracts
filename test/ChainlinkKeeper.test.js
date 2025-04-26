f2f614a481331617a4c3fbdd70ebf83c35d1e6e9
Vikalp Nag <vikalpnag@gmail.com>
2025-04-26 14:20:57 +0530

    test✅:correct initial state

--- test/ChainlinkKeeper.test.js
+++ test/ChainlinkKeeper.test.js
@@ -1,17 +1,21 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("ChainlinkKeeper contract", function () {
-  let keeperContract;
-  let interval;
-  let owner;
+// describe("ChainlinkKeeper contract", function () {
+//   let keeperContract;
+//   let interval;
+//   let owner;
 
-  beforeEach(async () => {
-    [owner] = await ethers.getSigners();
+//   beforeEach(async () => {
+//     [owner] = await ethers.getSigners();
 
-    interval = 5;
-    const Keeper = await ethers.getContractFactory("ChainlinkKeeper");
-    keeperContract = await Keeper.deploy(interval);
-    await keeperContract.waitForDeployment();
-  });
-});
+//     interval = 5;
+//     const Keeper = await ethers.getContractFactory("ChainlinkKeeper");
+//     keeperContract = await Keeper.deploy(interval);
+//     await keeperContract.waitForDeployment();
+//   });
+
+//   it("Initial state should be correct", async () => {
+//     expect(await keeperContract.counter()).to.equal(0);
+//   });
+// });
