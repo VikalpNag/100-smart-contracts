d421533937904a235f0e4aa8e062203ad3848f45
Vikalp Nag <vikalpnag@gmail.com>
2025-04-27 00:37:47 +0530

    test ✅ : decision request

--- test/AIOracle.test.js
+++ test/AIOracle.test.js
@@ -12,4 +12,20 @@
 //     aiOracle = await AIOracle.deploy();
 //     await aiOracle.waitForDeployment();
 //   });
+
+//   it("Should request a decision", async () => {
+//     const tx = await aiOracle.requestDecision();
+//     const receipt = await tx.wait();
+
+//     const event = receipt.logs.find(
+//       (log) => log.fragment.name === "DecisionRequested"
+//     );
+//     const decisionId = event.args.decisionId;
+
+//     expect(decisionId).to.be.equal(1);
+
+//     const decision = await aiOracle.decisions(decisionId);
+//     expect(decision.status).to.equal(0); //pending
+//     expect(decision.choice).to.equal(0); //unknown
+//   });
 // });
