cffb8be2c5eb4c004ec15f59b87dc0090dc78d3b
Vikalp Nag <vikalpnag@gmail.com>
2025-04-26 10:38:15 +0530

    ChainlinkKeeper contract

--- contracts/39ChainlinkKeeper.sol
+++ contracts/39ChainlinkKeeper.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-// import "@chainlink/contracts/src/vrf/interfaces/KeeperCompatibleInterface.sol";
-import "@chainlink/contracts/src/v0.8/automation/KeeperCompatible.sol";
+import "@chainlink/contracts/src/v0.8/automation/interfaces/AutomationCompatibleInterface.sol";
 
-contract ChainlinkKeeper is KeeperCompatible {
+contract ChainlinkKeeper is AutomationCompatibleInterface {
     uint256 public lastTimeStamp;
     uint256 public interval;
     uint256 public counter;
@@ -13,5 +12,24 @@ contract ChainlinkKeeper is KeeperCompatible {
         interval = _interval;
         lastTimeStamp = block.timestamp;
         counter = 0;
+    }
+
+    function checkUpkeep(
+        bytes calldata /* checkData */
+    )
+        external
+        view
+        override
+        returns (bool upkeepNeeded, bytes memory performData)
+    {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        performData = "";
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external override {
+        if ((block.timestamp - lastTimeStamp) > interval) {
+            lastTimeStamp = block.timestamp;
+            counter++;
+        }
     }
 }
