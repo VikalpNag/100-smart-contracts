c3b23ea66535b9f6d6ad50c7980a12563d15135d
Vikalp Nag <vikalpnag@gmail.com>
2025-04-27 00:24:43 +0530

    #beforeEach

--- test/AIOracle.test.js
+++ test/AIOracle.test.js
@@ -1,7 +1,15 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
 
-describe("AIOracle", function () {
-  let aiOracle;
-  let owner, otherUser;
-});
+// describe("AIOracle", function () {
+//   let aiOracle;
+//   let owner, otherUser;
+
+//   beforeEach(async () => {
+//     [owner, otherUser] = await ethers.getSigners();
+
+//     const AIOracle = await ethers.getContractFactory("AIOracle");
+//     aiOracle = await AIOracle.deploy();
+//     await aiOracle.waitForDeployment();
+//   });
+// });
