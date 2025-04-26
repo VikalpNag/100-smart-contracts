8b7aac5c2ce294bd597fd6e5436acd40e832e4dc
Vikalp Nag <vikalpnag@gmail.com>
2025-04-26 23:36:49 +0530

    Declared variables

---
+++ contracts/40AIOracle.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AIOracle {
+    enum DecisionStatus {
+        Pending,
+        Fulfilled
+    }
+    enum AIChoice {
+        Unknown,
+        Approve,
+        Reject
+    } //Example choices made by ai
+
+    struct Decision {
+        DecisionStatus status;
+        AIChoice choice;
+    }
+
+    address public owner;
+    mapping(uint256 => Decision) public decisions;
+    uint256 public decisionCounter;
+}
