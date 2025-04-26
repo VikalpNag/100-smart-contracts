98b98cf38b79ea8604b270b6c30af2293ab11a86
Vikalp Nag <vikalpnag@gmail.com>
2025-04-26 10:04:45 +0530

    fix keepercompatible import

--- contracts/39ChainlinkKeeper.sol
+++ contracts/39ChainlinkKeeper.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
+// import "@chainlink/contracts/src/vrf/interfaces/KeeperCompatibleInterface.sol";
+import "@chainlink/contracts/src/v0.8/automation/KeeperCompatible.sol";
 
-contract ChainlinkKeeper is KeeperCompatibleInterface {
+contract ChainlinkKeeper is KeeperCompatible {
     uint256 public lastTimeStamp;
     uint256 public interval;
     uint256 public counter;
