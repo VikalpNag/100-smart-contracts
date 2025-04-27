e8346844061606ff249a2fb69292511680e966a4
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 04:20:47 +0530

    created events

--- contracts/42CredentialVerifier.sol
+++ contracts/42CredentialVerifier.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CredintialVerifier {
-    mapping(address => bytes32) public credintials;
+contract CredentialVerifier {
+    mapping(address => bytes32) public credentials;
+
+    event CredintialStored(address indexed user, bytes32 credentialHash);
+    event CredentialVerified(address indexed user);
 }
