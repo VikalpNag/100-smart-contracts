e034d1639dda5c3a3f8188206006f9a803a70d05
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 02:46:36 +0530

    created function bridgeTokens()

--- contracts/41CrossChainBridge.sol
+++ contracts/41CrossChainBridge.sol
@@ -18,4 +18,21 @@ contract CrossChainBridge is Ownable {
     constructor(address tokenAddress) {
         token = IERC20(tokenAddress);
     }
+
+    //Called on Source Chain(Chain A)
+    function bridgeTokens(
+        uint256 amount,
+        uint256 targetChainId,
+        address targetAddress
+    ) external {
+        require(amount > 0, "Amount must be greater than 0");
+        require(targetAddress != address(0), "Invalid target address");
+
+        //Burn user's tokens (transfer to bridge)
+        require(
+            token.transferFrom(msg.sender, address(this), amount),
+            "Transfer Failed"
+        );
+        emit BridgeRequest(msg.sender, amount, targetChainId, targetAddress);
+    }
 }
