36abd929a959a4c3a13ea1cb6fa1da2722949a9c
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 03:03:31 +0530

    declared variables

---
+++ test/CrossChainBridge.test.js
@@ -0,0 +1,10 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("CrossChainBridge", function () {
+//   let owner, user, bridge, token;
+
+//   const initialSupply = ethers.parseEther("100000");
+//   const amountToBridge = ethers.parseEther("1000");
+
+// });
