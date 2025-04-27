3c7f8d5ce0dbddffb49733696bf02acc11cf7908
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 04:36:30 +0530

    declared test file

---
+++ test/CredentialVerifier.test.js
@@ -0,0 +1,8 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("CredentialVerifier", function () {
+//   let owner, user;
+//   let credentialVerifier;
+
+// });
