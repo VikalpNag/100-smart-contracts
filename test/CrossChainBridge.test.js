7e6a007e2699c55218b24731b81cdb6697e30a7d
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 03:36:29 +0530

    test ✅ : BridgeRequest Event

--- test/CrossChainBridge.test.js
+++ test/CrossChainBridge.test.js
@@ -1,22 +1,32 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("CrossChainBridge", function () {
-//   let owner, user, bridge, token;
+describe("CrossChainBridge", function () {
+  let owner, user, bridge, token;
 
-//   const initialSupply = ethers.parseEther("100000");
-//   const amountToBridge = ethers.parseEther("1000");
+  const initialSupply = ethers.parseEther("100000");
+  const amountToBridge = ethers.parseEther("1000");
 
-//   beforeEach(async () => {
-//     [owner, user] = await ethers.getSigners();
+  beforeEach(async () => {
+    [owner, user] = await ethers.getSigners();
 
-//     const ERC20Mintable = await ethers.getContractFactory("ERC20Mintable");
-//     token = await ERC20Mintable.deploy("Test Token", "TTK");
-//     await token.waitForDeployment();
-//     await token.mint(user.address, initialSupply);
+    const ERC20Mintable = await ethers.getContractFactory("DummyToken");
+    token = await ERC20Mintable.deploy("Test Token", "TTK");
+    await token.waitForDeployment();
+    await token.mint(user.address, initialSupply);
 
-//     const Bridge = await ethers.getContractFactory("CrossChainBridge");
-//     bridge = await Bridge.deploy(token.target);
-//     await bridge.waitForDeployment();
-//   });
-// });
+    const Bridge = await ethers.getContractFactory("CrossChainBridge");
+    bridge = await Bridge.deploy(token.target);
+    await bridge.waitForDeployment();
+  });
+
+  it("Should emit BridgeRequest event on bridgeTokens", async () => {
+    await token.connect(user).approve(bridge.target, amountToBridge);
+
+    await expect(
+      bridge.connect(user).bridgeTokens(amountToBridge, 137, user.address) //137=polygon chainId
+    )
+      .to.emit(bridge, "BridgeRequest")
+      .withArgs(user.address, amountToBridge, 137, user.address);
+  });
+});
