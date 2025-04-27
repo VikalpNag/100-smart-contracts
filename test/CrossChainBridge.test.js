5ed1a5b19f389ef70a890cea580c7beb2f204bc5
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 03:11:38 +0530

    Initiated test file

--- test/CrossChainBridge.test.js
+++ test/CrossChainBridge.test.js
@@ -7,4 +7,16 @@
 //   const initialSupply = ethers.parseEther("100000");
 //   const amountToBridge = ethers.parseEther("1000");
 
+//   beforeEach(async () => {
+//     [owner, user] = await ethers.getSigners();
+
+//     const ERC20Mintable = await ethers.getContractFactory("ERC20Mintable");
+//     token = await ERC20Mintable.deploy("Test Token", "TTK");
+//     await token.waitForDeployment();
+//     await token.mint(user.address, initialSupply);
+
+//     const Bridge = await ethers.getContractFactory("CrossChainBridge");
+//     bridge = await Bridge.deploy(token.target);
+//     await bridge.waitForDeployment();
+//   });
 // });
