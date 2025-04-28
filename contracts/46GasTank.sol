2cfe50c0b2f9c45c91af54c646d29dc1edbf92b4
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:46:28 +0530

    created events

--- contracts/46GasTank.sol
+++ contracts/46GasTank.sol
@@ -6,4 +6,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract GasTank is Ownable(msg.sender) {
     mapping(address => uint256) public balances;
     mapping(address => bool) public relayers;
+
+    event Deposited(address indexed user, uint256 amount);
+    event GasSpent(
+        address indexed user,
+        uint256 amount,
+        address indexed relayer
+    );
+    event RelayerUpdated(address indexed relayer, bool allowed);
 }
