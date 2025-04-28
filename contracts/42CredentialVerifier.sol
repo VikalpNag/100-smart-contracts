a63c20571943aff49cfdc00deffdc5b419ccf0fb
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 22:59:12 +0530

    fixed CredintialStored to CredentialStored

--- contracts/42CredentialVerifier.sol
+++ contracts/42CredentialVerifier.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.0;
 contract CredentialVerifier {
     mapping(address => bytes32) public credentials;
 
-    event CredintialStored(address indexed user, bytes32 credentialHash);
+    event CredentialStored(address indexed user, bytes32 credentialHash);
     event CredentialVerified(address indexed user);
 
     function storeCredential(bytes32 _credentialHash) external {
