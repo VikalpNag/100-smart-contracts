add63620360de334dd69f7695ad0549c74a2fe3a
Vikalp Nag <vikalpnag@gmail.com>
2025-04-28 22:50:01 +0530

    created function mint()

--- contracts/43IdentityNFT.sol
+++ contracts/43IdentityNFT.sol
@@ -21,4 +21,12 @@ contract IdentityNFT is ERC721URIStorage, Ownable {
     function setVerifier(address _verifier, bool _status) external onlyOwner {
         verifiers[_verifier] = _status;
     }
+
+    function mint(address to, string memory uri) external onlyVerifier {
+        uint256 tokenId = nextTokenId;
+        nextTokenId++;
+
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
 }
