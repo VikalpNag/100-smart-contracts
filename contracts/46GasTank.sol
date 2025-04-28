bb8246cbf9b344b89267160dc62f7c5a9e6bf910
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:45:45 +0530

    declared variables

---
+++ contracts/46GasTank.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GasTank is Ownable(msg.sender) {
+    mapping(address => uint256) public balances;
+    mapping(address => bool) public relayers;
+}
