e342676fee8a1b8917da36d916f068cbce1f3c7b
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:36:40 +0530

    test initiated

--- test/AutoRaffle.test.js
+++ test/AutoRaffle.test.js
@@ -3,4 +3,12 @@
 
 // describe("AutoRaffle", function () {
 //   let raffle, owner, user1, user2, user3;
+
+//   beforeEach(async () => {
+//     [owner, user1, user2, user3] = await ethers.getSigners();
+
+//     const AutoRaffle = await ethers.getContractFactory("AutoRaffle");
+//     raffle = await AutoRaffle.deploy();
+//     await raffle.waitForDeployment();
+//   });
 // });
