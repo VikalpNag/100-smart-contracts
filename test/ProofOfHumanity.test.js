d6ba6c33e18ea211ba08f843230ff6f70b6b7dba
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 01:10:01 +0530

    Initiated Test

--- test/ProofOfHumanity.test.js
+++ test/ProofOfHumanity.test.js
@@ -4,4 +4,11 @@
 // describe("ProofOfHumanity", function () {
 //   let proofOfHumanity, owner, user1, user2;
 
+//   beforeEach(async () => {
+//     [owner, user1, user2] = await ethers.getSigners();
+
+//     const ProofOfHumanity = await ethers.getContractFactory("ProofOfHumanity");
+//     proofOfHumanity = await ProofOfHumanity.deploy();
+//     await proofOfHumanity.waitForDeployment();
+//   });
 // });
