9d39e8f2f48586ccac4f823a811cbd368070bc8a
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:57:24 +0530

    test ✅ : user deposit ETH

--- test/GasTank.test.js
+++ test/GasTank.test.js
@@ -3,4 +3,21 @@
 
 // describe("GasTank", function () {
 //   let gasTank, owner, user1, user2, relayer;
+
+//   beforeEach(async () => {
+//     [owner, user1, user2, relayer] = await ethers.getSigners();
+
+//     const GasTank = await ethers.getContractFactory("GasTank");
+//     gasTank = await GasTank.deploy();
+//     await gasTank.waitForDeployment();
+//   });
+
+//   it("Allows users to deposit ETH", async () => {
+//     await gasTank
+//       .connect(user1)
+//       .depositForUser(user1.address, { value: ethers.parseEther("1") });
+
+//     const balance = await gasTank.userBalance(user1.address);
+//     expect(balance).to.equal(ethers.parseEther("1"));
+//   });
 // });
