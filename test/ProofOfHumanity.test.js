45b15796b98435cd768e37852248d729be18bbb7
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 01:07:34 +0530

    declared variables

---
+++ test/ProofOfHumanity.test.js
@@ -0,0 +1,7 @@
+// const { expect } = require("chai");
+// const { ethers } = require("hardhat");
+
+// describe("ProofOfHumanity", function () {
+//   let proofOfHumanity, owner, user1, user2;
+
+// });
