4d098dc8be29f2ade8ff1d5f1f9859c2ef18de2e
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:38:25 +0530

    test ✅ : raffle entry

--- test/AutoRaffle.test.js
+++ test/AutoRaffle.test.js
@@ -11,4 +11,14 @@
 //     raffle = await AutoRaffle.deploy();
 //     await raffle.waitForDeployment();
 //   });
+
+//   it("Users can enter raffle", async () => {
+//     await raffle.connect(user1).enterRaffle({ value: ethers.parseEther("1") });
+//     await raffle.connect(user2).enterRaffle({ value: ethers.parseEther("2") });
+
+//     const participants = await raffle.getParticipants();
+//     expect(participants).to.have.lengthOf(2);
+//     expect(participants[0]).to.equal(user1.address);
+//     expect(participants[1]).to.equal(user2.address);
+//   });
 // });
