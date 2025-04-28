2347f03274b02a0770500622a33b2dd76eeae7b8
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 00:38:50 +0530

    created variables

---
+++ contracts/44ProofOfHumanity.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+contract ProofOfHumanity is Ownable {
+    mapping(address => bool) public isRegistered;
+    uint256 public totalHumans;
+}
