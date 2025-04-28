eebdd9c3abc6e2ee6bbd94e855e34cffcd27de7f
Vikalp Nag <vikalpnag@gmail.com>
2025-04-29 02:18:03 +0530

    declared variables

---
+++ contracts/45AutoRaffle.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract AutoRaffle is Ownable(msg.sender) {
+    address[] public participants;
+    bool public raffleOpen = true;
+    address public recentWinner;
+}
