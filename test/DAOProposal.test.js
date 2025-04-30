04599c2c850cdf7ca7cd4914baeebca809c6440c
Vikalp Nag <vikalpnag@gmail.com>
2025-04-30 22:06:17 +0530

    initiated test

--- test/DAOProposal.test.js
+++ test/DAOProposal.test.js
@@ -1,6 +1,14 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("DAOProposal", async () => {
-//   let DAO, dao, owner, voter1, voter2;
-// });
+describe("DAOProposal", async () => {
+  let DAO, dao, owner, voter1, voter2;
+
+  beforeEach(async () => {
+    [owner, voter1, voter2] = await ethers.getSigners();
+
+    DAO = await ethers.getContractFactory("DAOProposal");
+    dao = await DAO.deploy(2); //quorum=2
+    await dao.waitForDeployment();
+  });
+});
