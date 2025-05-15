4cf5e7558f251c0763b34817cff96844ceaa512b
Vikalp Nag <vikalpnag@gmail.com>
2025-05-15 12:35:33 +0530

    test failing

--- test/DAOProposal.test.js
+++ test/DAOProposal.test.js
@@ -1,27 +1,37 @@
-// const { expect } = require("chai");
-// const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-// describe("DAOProposal", async () => {
-//   let DAO, dao, owner, voter1, voter2;
+describe("DAOProposal", () => {
+  let DAO, dao, owner, voter1, voter2;
 
-//   beforeEach(async () => {
-//     [owner, voter1, voter2] = await ethers.getSigners();
+  beforeEach(async () => {
+    [owner, voter1, voter2] = await ethers.getSigners();
 
-//     DAO = await ethers.getContractFactory("DAOProposal");
-//     dao = await DAO.deploy(2); //quorum=2
-//     await dao.waitForDeployment();
-//   });
+    DAO = await ethers.getContractFactory("DAOProposal");
+    dao = await DAO.deploy(2); // quorum = 2
+    await dao.waitForDeployment();
+  });
 
-//   it("Should create a new proposal", async () => {
-//     await expect(dao.createProposal("Fund open-source devs"))
-//       .to.emit(dao, "ProposalCreated")
-//       .withArgs(
-//         0,
-//         "Fund open-source devs",
-//         (await dao.getBlockTimestamp()) + 3 * 24 * 60 * 60
-//       );
+  it("Should create a new proposal", async () => {
+    const description = "Fund open-source devs";
 
-//     const proposal = await dao.proposals(0);
-//     expect(proposal.description).to.equal("Fund open-source devs");
-//   });
-// });
+    const tx = await dao.createProposal(description);
+    const receipt = await tx.wait();
+
+    const event = receipt.events.find(e => e.event === "ProposalCreated");
+    const [id, desc, deadline] = event.args;
+
+    expect(id).to.equal(0);
+    expect(desc).to.equal(description);
+
+    const proposal = await dao.proposals(0);
+    expect(proposal.description).to.equal(description);
+
+    // Check that deadline is approx 3 days from now (allowing ±2s diff)
+    const currentBlock = await ethers.provider.getBlock(receipt.blockNumber);
+    const expectedDeadline = BigInt(currentBlock.timestamp) + BigInt(3 * 24 * 60 * 60);
+    const actualDeadline = deadline;
+
+    expect(actualDeadline).to.be.closeTo(expectedDeadline, 2n); // ±2 seconds tolerance
+  });
+});
