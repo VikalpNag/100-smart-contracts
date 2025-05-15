34fb988483b8d1f0593b2cdab075e78c91d13f1a
Vikalp Nag <vikalpnag@gmail.com>
2025-05-15 22:50:15 +0530

    declared vars

---
+++ contracts/48GovernanceToken.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract GovernanceToken is ERC20Votes, Ownable {
+    uint256 public maxSupply = 1000000 * 1e18;
+}
