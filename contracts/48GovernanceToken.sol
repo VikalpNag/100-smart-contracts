0fa795c52fdc62d0509e3ce3c6b42ab25738910b
Vikalp Nag <vikalpnag@gmail.com>
2025-05-15 22:53:13 +0530

    created constructor

--- contracts/48GovernanceToken.sol
+++ contracts/48GovernanceToken.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GovernanceToken is ERC20Votes, Ownable {
+contract GovernanceToken is ERC20Votes, Ownable(msg.sender) {
     uint256 public maxSupply = 1000000 * 1e18;
+
+    constructor()
+        ERC20("GovernanceToken", "GT")
+        ERC20Permit("GovernanceToken")
+    {
+        _mint(msg.sender, maxSupply);
+    }
 }
