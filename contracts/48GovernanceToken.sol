0a6c25bf0b3e73d18614c3f054411fa6006c57a4
Vikalp Nag <vikalpnag@gmail.com>
2025-05-16 11:27:25 +0530

    Add: Functions _afterTokenTransfer,_mint

--- contracts/48GovernanceToken.sol
+++ contracts/48GovernanceToken.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
-
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract GovernanceToken is ERC20Votes, ERC20Permit, Ownable(msg.sender) {
@@ -15,4 +14,20 @@ contract GovernanceToken is ERC20Votes, ERC20Permit, Ownable(msg.sender) {
     {
         _mint(msg.sender, maxSupply);
     }
+
+    //The Function below overrides required by Solidity for ERC20Votes
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount);
+    }
+
+    function _mint(
+        address to,
+        uint256 amount
+    ) internal override(ERC20, ERC20Votes){
+        super._mint(to,amount);
+    };
 }
